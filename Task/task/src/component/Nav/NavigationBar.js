3567a5cfb16e6d4e47606d60ca5eb9b4245a1ec6
kasunWK <90896335+kasunWK@users.noreply.github.com>
2024-05-03 23:11:09 +0530

    updated

--- Task/task/src/component/Nav/NavigationBar.js
+++ Task/task/src/component/Nav/NavigationBar.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import './NavigationBar.css';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavigationBar.css';
 
 const NavigationBar = () => {
   return (
-    <nav className="navbar">
-      <div className="nav-options">
-        <img src='./img/logo.png' alt='logo'/>
-        <a href="/about-us">ABOUT US</a>
-        <a href="/math">MATH</a>
-      </div>
-      <div className="nav-contact">
-        <img src='./img/contact.png' alt='contact information image'/>
-      </div>
-      <Link to="/" className="login-button">Login</Link>
-    </nav>
+    <Navbar bg="light" expand="lg" className="navbar">
+      <Navbar.Brand>
+        <img src='./img/logo.png' alt='logo' className='logo'/>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/about-us">
+            <Button variant="primary" className='nav-button'>ABOUT</Button>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/math">
+            <Button variant="primary" className='nav-button'>MATH</Button>
+          </Nav.Link>
+        </Nav>
+        <div>
+          <img src='./img/contact.png' alt='contact-info' className='contact-i'/>
+        </div>
+      </Navbar.Collapse>
+      <Link to="/" className="login-button">
+        <Button variant="outline-primary">Login</Button>
+      </Link>
+    </Navbar>
   );
 };
 
 export default NavigationBar;
-
-
-
